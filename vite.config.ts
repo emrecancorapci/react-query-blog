39a42deabe27dccc50636629d3f4ac43be95172b
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-07-16 16:58:45 +0300

    package updates, eslint changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslintPlugin from '@nabla/vite-plugin-eslint';
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  eslintPlugin()],
+  plugins: [react(),  eslintPlugin(), tsConfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
